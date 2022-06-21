cad57d7164c894e54d96f461aadd0e5c70d7ac0b
Luss <nhok8rytaband@gmail.com>
2022-06-21 17:51:05 +0700

    commit test 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+function App()
+{
+    return(
+        <>
+        <Header />
+        <div className="container">
+            <h1>MyApp</h1>
+        </div>
+        </>
+    )
+}
+export default App;
